a1ba8492a391a693c8973bded53c9cf152110151
RihardsMacijauskis <rihardsmacijauskis@gmail.com>
2022-10-30 16:30:19 +0200

    Showcase Cypress interceptions

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,15 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "scd26m",
   e2e: {
     chromeWebSecurity: false,
+    reporter: "mochawesome",
+    reporterOptions: {
+      reportDir: "cypress/results",
+      html: false,
+      overwrite: false,
+    },
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
