43bd9c6ce98b8824faaac0e7b71be8d29cbc5eef
RihardsMacijauskis <rihardsmacijauskis@gmail.com>
2022-10-30 11:00:21 +0200

    Add remove item case

--- cypress/e2e/day1/products.cy.js
+++ cypress/e2e/day1/products.cy.js
@@ -4,11 +4,18 @@ import {ProductsPage} from "../../pageObjects/productsPage";
 describe("Adding and Removing items from the products screen" , () => {
 
     it("Adding first item visible to the cart" , () => {
-        LoginPage.loginStandardUserWithoutUI()
+        LoginPage.loginStandardUserWithoutUI("https://www.saucedemo.com/inventory.html")
         ProductsPage.addFirstItemToCart()
         ProductsPage.verifyAddedItemAmount(1)
         ProductsPage.openCart()
         ProductsPage.verifyLastAddedItemData()
+    })
+
+    it("Removing items from the cart", () => {
+        ProductsPage.setupCartForTests()
+        LoginPage.loginStandardUserWithoutUI("https://www.saucedemo.com/cart.html")
+        ProductsPage.removeItemsFromCart()
+        ProductsPage.verifyCartIsEmpty()
     })
 
 })
